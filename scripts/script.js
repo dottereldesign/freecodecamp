cf53fa9b359b1ad3946c1a46090f58b4e6a378e9
Jamie Wilson <howemanning@gmail.com>
2022-02-22 22:53:55 +1300

    Progressed to 25:18min - Escape Sequence in Strings.

---
+++ scripts/script.js
@@ -0,0 +1,138 @@
+// Copy Line Up/Down - On Windows: Shift + Alt + Up/Down
+// camelCase
+// Colonize extension - Alt + Enter will insert semi-colon and move to next line.
+
+// Data Types and Variables
+var myName = "Beau";
+myName = 8;
+let ourName = "freeCodeCamp";
+const pi = 3.14;
+
+// Storing Values with Assignment Operator
+var a;
+var b = 2;
+console.log(a);
+a = 7;
+b = a;
+console.log(a);
+
+// Initilizing Variables with the Assignment Operator
+var a = 9;
+console.log(a);
+
+// Uninitilized Variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+console.log(a);
+console.log(b);
+console.log(c);
+
+var sentence = "Hello, my name is";
+sentence = sentence + " Jamie.";
+console.log(sentence);
+
+// Adding Numbers
+var sum = 10 + 10;
+console.log(sum);
+
+// Subtracting Numbers
+var difference = 45 - 33;
+console.log(difference);
+
+// Multiplying Numbers
+var product = 8 * 10;
+console.log(product);
+
+// Diving Numbers
+var quotient = 66 / 33;
+console.log(quotient);
+
+// Incrementing Numbers
+var myVar = 87;
+myVar++;
+console.log(myVar);
+
+// Decrementing Numbers
+var myVar = 11;
+myVar--;
+console.log(myVar);
+
+// Decimal Numbers
+var ourDecimal = 5.7;
+console.log(ourDecimal);
+var myDecimal = 0.009;
+console.log(myDecimal);
+
+// Multiply Decimals
+var product = 2.0 * 2.5;
+console.log(product);
+
+// Divide Decimals
+var quotient = 4.4 / 2.0;
+console.log(quotient);
+
+// Finding a Remainder
+var remainder;
+remainder = 11 % 3;
+console.log(remainder);
+
+// Compound Assignment with Augmented Addition
+var a = 3;
+var b = 17;
+var c = 12;
+a += 12;
+b += 9;
+c += 7;
+console.log(a);
+console.log(b);
+console.log(c);
+
+// Compound Assignment with Augmented Subtration
+var a = 11;
+var b = 9;
+var c = 3;
+a -= 6;
+b -= 15;
+c -= 1;
+console.log(a);
+console.log(b);
+console.log(c);
+
+// Compound Assignment with Augmented Multiplication
+var a = 5;
+var b = 12;
+var c = 4.6;
+a *= 5;
+b *= 3;
+c *= 10;
+console.log(a);
+console.log(b);
+console.log(c);
+
+// Compound Assignment with Augmented Division
+var a = 488;
+var b = 108;
+var c = 33;
+a /= 12;
+b /= 4;
+c /= 11;
+console.log(a);
+console.log(b);
+console.log(c);
+
+// Declare String Variables
+var firstName = "Alan";
+var lastName = "Turing";
+firstName = "Jamie";
+lastName = "Wilson";
+console.log(firstName, lastName);
+
+// Quoting Strings with Single Quotes
+var myStr = 'I am a "double quoted" string inside "double quotes"';
+console.log(myStr);
+
+// Escape Sequences in Strings
